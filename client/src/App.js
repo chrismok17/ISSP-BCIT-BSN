79465ca4c5730575fb817dcf786506e3f164a824
Simon <sfreeman15@my.bcit.ca>
2023-02-02 20:29:48 -0800

    Added boiletplate text to the Calendar pop-up

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import { useState } from 'react';
-import Calendar from 'react-calendar';
-import './App.css';
-import 'react-calendar/dist/Calendar.css';
-import PopUp from './components/PopUp';
-
+import logo from "./logo.svg";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "./App.css";
+import "react-calendar/dist/Calendar.css";
+import PopUp from "./components/PopUp";
 
 function App() {
-  const [value, onChange] = useState(new Date())
-  const [ popUpOpen, setPopUpOpen] = useState(false)
+  const [value, onChange] = useState(new Date());
+  const [popUpOpen, setPopUpOpen] = useState(false);
 
   return (
     <div className="App">
       <Calendar
         onChange={onChange}
-        value={value} 
+        value={value}
         onClickDay={(value, event) => {
-          console.log("clicked.", value, event)
-          setPopUpOpen(!popUpOpen)
+          console.log("clicked.", value, event);
+          setPopUpOpen(!popUpOpen);
         }}
-        />
+      />
       {popUpOpen && <PopUp />}
     </div>
   );
--- client/src/components/PopUp.js
+++ client/src/components/PopUp.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-export default function PopUp () {
-    return (
-    <div> popup hello</div>
-    )
+export default function PopUp() {
+  return (
+    <div>
+      <p>
+        <b>Jasica Munday</b>
+      </p>
+      <p>
+        <b>10:00-11:00 AM</b>
+      </p>
+      <p>
+        <b>Building NW 4</b>
+      </p>
+    </div>
+  );
 }
