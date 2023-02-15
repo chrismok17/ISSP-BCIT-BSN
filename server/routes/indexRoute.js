cb7ab77e59d5bb2855df8572c523616d52935a79
hfatima76 <hfatima@my.bcit.ca>
2023-02-15 14:21:14 -0800

    added route to user sign up and redirection to other pages

--- server/routes/indexRoute.js
+++ server/routes/indexRoute.js
@@ -1,58 +1,95 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
-const { forwardAuthenticated } = require("../middleware/checkAuth");
+// const { forwardAuthenticated } = require("../middleware/checkAuth");
+const { checkNotAuthenticated } = require("../middleware/checkAuth")
 const passport = require("../middleware/passport");
 
 
-router.get("/", forwardAuthenticated, (req, res) => res.render("login"));
+router.get("/", ensureAuthenticated, (req, res) => 
+res.render("dashboard.ejs"));
 
-// Previous code, ignore
+router.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
 
-// app.use('/login', (req, res) => {
-//   res.send({
-//     token: 'test123'
-//   });
-// });
+router.get("/signup", checkNotAuthenticated, (req, res) => {
+  res.render("signup.ejs");
+});
+
 
 // Created /login endpoint to get login values from front end
 // Trying to get the values to be compared with values from mysql
 // If successful please send status 200
 
-router.post("/login", (req, res, next) => {
-    passport.authenticate("local", (err, user, info) => {
-      if (err) {
-        return next(err)
-      }
-      if (!user) {
-        return res.status(401).send({message: info.message})
-      }
-      req.logIn(user, (err) => {
-        if (err) {
-          return next(err);
-        } 
-        return res.status(200).send({message: "Success"})
-      })
-    })(req, res, next)
+// router.post("/login", (req, res, next) => {
+//     passport.authenticate("local", (err, user, info) => {
+//       if (err) {
+//         return next(err)
+//       }
+//       if (!user) {
+//         return res.status(401).send({message: info.message})
+//       }
+//       req.logIn(user, (err) => {
+//         if (err) {
+//           return next(err);
+//         } 
+//         return res.status(200).send({message: "Success"})
+//       })
+//     })(req, res, next)
+// })
+
+
+router.post("/login", checkNotAuthenticated ,(req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })(req, res, next);
+});
+
+
+// router.post(
+//   "/",
+//   passport.authenticate("local", {
+//     successRedirect: "/dashboard",
+//     failureRedirect: "/",
+//   })
+// );
+
+
+
+// redirection to the calendar page
+router.get('/calendar', (req, res) => {
+  res.render('calendar');
 })
 
-router.post(
-  "/",
-  passport.authenticate("local", {
-    successRedirect: "/dashboard",
-    failureRedirect: "/",
-  })
-);
 
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
-});
+router.post('/signup', checkNotAuthenticated, async(req, res)=> { 
+  //add code here for inserting new user in to the database
+  // const email= req.bosy.email
+  // const password= req.bosy.password
+  // const password2= req.bosy.password2
+})
 
-router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  res.render("dashboard", {
-    user: req.user,
+
+// logout function
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err){
+      if(err) return next(err);
+      res.redirect('/login');
   });
 });
+
+// router.get("/logout", (req, res) => {
+//   req.logout();
+//   res.redirect("/");
+// });
+
+// router.get("/dashboard", ensureAuthenticated, (req, res) => {
+//   res.render("dashboard", {
+//     user: req.user,
+//   });
+// });
 
 module.exports = router;
